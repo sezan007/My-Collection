0fdced81d70a543c0225609a6acb61a04cdf02c1
sezan <naimulsezan@gmail.com>
2024-05-08 02:52:17 +0600

    Trying with existing user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def home
+    @users = User.all # Assuming User is your model representing users
+  end
+  def users
+  end
+
+  def admins
   end
 end
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,8 +1,13 @@
 require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get pages_home_url
+  test "should get users" do
+    get pages_users_url
+    assert_response :success
+  end
+
+  test "should get admins" do
+    get pages_admins_url
     assert_response :success
   end
 end
